1060f79af12776d220c265e96f540b7468e3e38c
Gerry Agbobada <gagbobada+git@gmail.com>
2020-04-12 01:50:38 +0200

    Include part of answers to exercises
    I misunderstood the try function during monadic treatment, that's
    something to check for. Also, at this point I'd start to need tests for
    all the refactoring and testing I'm doing. But I prefer doing it for
    xile at the moment.

--- src/HaskellScheme/Parser.hs
+++ src/HaskellScheme/Parser.hs
@@ -9,7 +9,7 @@ import           Text.ParserCombinators.Parsec
                                          hiding ( spaces )
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>@^_~?"
+symbol = oneOf "!$%&|*+-/:<=>@^_~?"
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -22,13 +22,16 @@ parseStringChar = noneOf "\"\\" <|> (char '\\' >> parseEscapedChar)
 
 parseCharLiteral :: Parser LispVal
 parseCharLiteral = do
-  _       <- string "#\\"
-  charLit <- many (noneOf " ")
-  return $ case charLit of
-    "space"   -> Character ' '
-    "newline" -> Character '\n'
-    ""        -> Character ' '
-    _         -> Character (charLit !! 0)
+  _ <- try $ string "#\\"
+  charLit <- try (string "newline" <|> string "space") <|> do
+    x <- anyChar
+    _ <- notFollowedBy alphaNum
+    return [x]
+  return $ Character $ case charLit of
+    "space"   -> ' '
+    "newline" -> '\n'
+    ""        -> ' '
+    _         -> (charLit !! 0)
 
 parseString :: Parser LispVal
 parseString = do
@@ -37,29 +40,64 @@ parseString = do
   _ <- char '"'
   return $ String x
 
+parseBool :: Parser LispVal
+parseBool = do
+  _ <- char '#'
+  (char 't' >> return (Bool True)) <|> (char 'f' >> return (Bool False))
+
 parseAtom :: Parser LispVal
 parseAtom = do
   first <- letter <|> symbol
   rest  <- many (letter <|> digit <|> symbol)
   let atom = first : rest
-  return $ case atom of
-    "#t" -> Bool True
-    "#f" -> Bool False
-    _    -> Atom atom
+  return $ Atom atom
 
 -- TODO : Parse floats / decimal
 -- TODO : Parse entire numerical tower
 parseNumber :: Parser LispVal
 parseNumber =
   ((many1 digit) >>= (return . Number . read))
-    <|> (   (string "#o")
-        >>  (many1 digit)
-        >>= (return . Number . fst . (!! 0) . readOct)
-        )
-    <|> (   (string "#x")
-        >>  (many1 digit)
-        >>= (return . Number . fst . (!! 0) . readHex)
-        )
+    <|> parseDecimalAlt
+    <|> parseHex
+    <|> parseOct
+    <|> parseBin
+
+parseDecimalAlt :: Parser LispVal
+parseDecimalAlt = do
+  _ <- try $ string "#d"
+  x <- many1 digit
+  (return . Number . read) x
+
+parseHex :: Parser LispVal
+parseHex = do
+  _ <- try $ string "#x"
+  x <- many1 hexDigit
+  return $ Number (hex2dig x)
+
+parseOct :: Parser LispVal
+parseOct = do
+  _ <- try $ string "#o"
+  x <- many1 octDigit
+  return $ Number (oct2dig x)
+
+parseBin :: Parser LispVal
+parseBin = do
+  _ <- try $ string "#b"
+  x <- many1 (oneOf "10")
+  return $ Number (bin2dig x)
+
+oct2dig :: String -> Integer
+oct2dig x = fst $ readOct x !! 0
+
+hex2dig :: String -> Integer
+hex2dig x = fst $ readHex x !! 0
+
+bin2dig :: String -> Integer
+bin2dig = bin2dig' 0
+bin2dig' :: Num t => t -> [Char] -> t
+bin2dig' digint "" = digint
+bin2dig' digint (x : xs) =
+  let old = 2 * digint + (if x == '0' then 0 else 1) in bin2dig' old xs
 
 parseList :: Parser LispVal
 parseList = liftM List $ sepBy parseExpr spaces
@@ -93,8 +131,9 @@ parseExpr :: Parser LispVal
 parseExpr =
   parseAtom
     <|> parseString
-    <|> parseNumber
-    <|> parseCharLiteral
+    <|> try parseNumber
+    <|> try parseCharLiteral
+    <|> try parseBool
     <|> parseQuoted
     <|> parseUnquote
     <|> parseQuasiquoted
